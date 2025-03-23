a28fe7ac76dc143722bbf133c62108380a5da2de
qc teacher <quincycodersteacher@gmail.com>
2025-03-23 15:00:35 -0400

    a

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,9 +1,54 @@
-var canvas = document.querySelector('canvas');
 
-canvas.width = window.innerWidth-50;
-canvas.height = window.innerHeight-50;
+let canvas = document.querySelector("canvas");
+let drawingSurface = canvas.getContext("2d");
 
-var c = canvas.getContext('2d');
-c.fillRect(100,100,100,100);
-c.fillRect(400,100,100,100);
-//c.fillRect(300,300,100,100);
+const cat = {
+    x: 100,
+    y: 100,
+    width: 64,
+    height: 64
+};
+
+const image = new Image();
+image.addEventListener("load", update, false);
+image.src = "images/cat.jpg";
+
+const speed = 3;
+let xSpeed = speed;
+let ySpeed = speed;
+let text = "Bouncy cat";
+
+function update() {
+    cat.x += xSpeed;
+    cat.y += ySpeed;
+    if (cat.width + cat.x > canvas.width) {
+        xSpeed = -speed;
+        text = "Going Left";
+    }
+    else if (cat.x < 1) {
+        xSpeed = speed;
+        text = "Going Right";
+    }
+    else if (cat.height + cat.y > canvas.height) {
+        ySpeed = -speed;
+        text = "Going Up";
+    }
+    else if (cat.y < 1) {
+        ySpeed = speed;
+        text = "Going Down"
+    }
+    render();
+    window.requestAnimationFrame(update, canvas);
+}
+
+function render() {
+    drawingSurface.clearRect(0, 0, canvas.width, canvas.height);
+    drawingSurface.drawImage
+        (
+            image,
+            cat.x, cat.y,
+            cat.height, cat.width
+        );
+    drawingSurface.font = "bold 20px Arial";
+    drawingSurface.fillText(text, 10, 30, 200);
+}
